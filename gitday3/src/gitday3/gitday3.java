5e3316c70e90db453a2f9799e67dbb73c96b7a2b
mansiasati <mansiasati10112000@gmail.com>
2024-07-31 12:47:04 +0530

    coomit morning

---
+++ gitday3/src/gitday3/gitday3.java
@@ -0,0 +1,18 @@
+package gitday3;
+
+public class gitday3 {
+
+	
+		public void m1()
+		{
+			System.out.println("m1 method");
+		}
+		public void m2()
+		{
+			System.out.println("m2 method ...........................");
+		}
+		
+		
+}
+	
+
